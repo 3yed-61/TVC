1fbce6f2ff00c9f27894dd96fab9e1214b88f34c
3λΞĐ <122279300+3yed-61@users.noreply.github.com>
2024-06-26 13:10:47 +0330

    Update functions.php

--- lite/functions.php
+++ lite/functions.php
@@ -338,5 +338,5 @@ function hiddifyHeader ($subscriptionName) {
         "#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531\n" .
         "#support-url: https://github.com/3yed-61\n";
 
-";
+
 }
